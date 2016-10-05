9359090ed33988b82b45be169b8e7977215af163
SamuelWr <samuel.wraak@gmail.com>
2016-10-05 16:36:50 +0200

    added aaah

---
+++ aaah/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace aaah
+{
+    class ah
+    {
+        public static void Main(string[] args)
+        {
+            string jon = Console.ReadLine();
+            string doc = Console.ReadLine();
+            if (jon.Length >= doc.Length)
+                Console.WriteLine("go");
+            else
+                Console.WriteLine("no");
+
+
+        }
+    }
+}
